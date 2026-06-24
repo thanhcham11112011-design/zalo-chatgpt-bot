a348fc9ce9f791b1949b55cc1a4ee76893b176bc
thanhcham11112011-design <thanhcham11112011@gmail.com>
2026-06-24 16:34:31 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot Công an phường Phù Liễn đang hoạt động!"
+
+@app.route("/health")
+def health():
+    return {"status": "ok"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
